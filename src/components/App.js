acd8e349d9643b9daf8477f5e4b257fa25fc30a3
Modolo Thomas <thomas.modolo@ynov.com>
2021-04-27 15:23:52 +0200

    creating navbar and todo component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { FaListAlt, FaCheckSquare, FaPlusSquare, FaTrash } from 'react-icons/fa';
-
+import ToDoList from "./ToDoList"
+import NavBar from './NavBar'
 
 
 class App extends React.Component {
@@ -8,32 +8,8 @@ class App extends React.Component {
         return (
             <section id="todo">
                 <h1 className="m-3">Liste de tâches</h1>
-                <ul className="list-group m-3">
-                    <li className="list-group-item d-flex align-tiems-center">
-                        Ranger la vaisselle
-                    <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-                    </li>
-                    <li className="list-group-item d-flex align-tiems-center">
-                        Répondre appel d'offres
-                    <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-                    </li>
-                    <li className="list-group-item d-flex align-tiems-center">
-                        Signer contrat
-                    <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-                    </li>
-                    <li className="list-group-item d-flex align-tiems-center">
-                        Ranger la salon
-                    <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-                    </li>
-                </ul>
-                <footer className="d-flex justify-content-between bg-secondary p-3" id="mainFooter">
-                    <div className="btn-group">
-                        <a href="#" className="btn btn-outline-dark bg-light"><FaListAlt></FaListAlt> </a>
-                        <a href="#" className="btn btn-outline-dark bg-light"><FaCheckSquare></FaCheckSquare></a>
-                        <a href="#" className="btn btn-outline-dark bg-light"><FaPlusSquare></FaPlusSquare></a>
-                    </div>
-                    <button className="btn btn-outline-dark bg-light"><FaTrash></FaTrash></button>
-                </footer>
+                <ToDoList />
+                <NavBar />
             </section>
         )
     }
